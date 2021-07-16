2078dafb648afac0a1d18cb4c12f12801a8f7b58
Taiyanna <simstaiyanna@yahoo.com>
2021-07-16 18:41:21 -0400

    Functions

--- app.js
+++ app.js
@@ -2,6 +2,33 @@ console.log("Hello World!\n==========\n");
 
 // Exercise 1 Section
 console.log("EXERCISE 1:\n==========\n");
+function printOdds(count){
+    for( let i= 1; i <= count; i++)
+    
+    if (i % 2 == 1) {
+        console.log(i)
+    } else {
+        console.log("not odd")
+    }
+   
+}
+printOdds(5)
 
 // Exercise 2 Section
 console.log("EXERCISE 2:\n==========\n");
+function checkAge(age){
+let aboveSixteen="You can drive!"
+let belowSixteen ="Sorry, you have to wait until you're 16."
+if (age< 16) {
+    console.log(belowSixteen)
+} else {
+    console.log(aboveSixteen)
+}
+}
+checkAge(15);
+
+
+function showDistance(speed,time){
+    console.log(speed* time);
+}
+ showDistance(10,5);
